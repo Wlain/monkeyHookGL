00ff187184543a8e20befb81be4ba8d0461f9549
chenweibiao <chenweibiao@kuaishou.com>
2021-04-25 17:19:24 +0800

    新增保存shader的接口

--- monkeyHookGL/monkeyHookGLDylib/monkeyHookGLDylib.mm
+++ monkeyHookGL/monkeyHookGLDylib/monkeyHookGLDylib.mm
@@ -17,23 +17,55 @@
 #import <MDCycriptManager.h>
 #import <vector>
 #import <GLKit/GLKit.h>
+#import <string>
+#import <string.h>
 
 
 static void(*glDrawArraysFunc)(GLenum mode, GLint first, GLsizei count);
 static void glDrawArraysNew(GLenum mode, GLint first, GLsizei count)
 {
-    NSLog(@"glDrawArrays called");
-//    glClearColor(1, 0, 0, 1);
-//    glClear(GL_COLOR_BUFFER_BIT);
-    glDrawArraysFunc(mode, first, count);
+    NSLog(@"glDrawArrays(%d, %d, %d) called", mode, first, count);
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+//    glDrawArraysFunc(mode, first, count);
 }
 
-void (*glDrawElementsFun)(GLenum mode, GLsizei count, GLenum type, const GLvoid* indices);
+static void (*glDrawElementsFun)(GLenum mode, GLsizei count, GLenum type, const GLvoid* indices);
 static void glDrawElementsNew(GLenum mode, GLsizei count, GLenum type, const GLvoid* indices)
 {
-    NSLog(@"glDrawElementsNew called");
-    glDrawElementsFun(mode, count, type, indices);
+    NSLog(@"glDrawElementsNew(%d, %d, %d, %p) called", mode, count, type, indices);
+//    glDrawElementsFun(mode, count, type, indices);
 }
+
+static int shaderIndex = 0;
+static void saveShaderToFile(NSString* source)
+{
+    NSString* documentPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES)[0];
+    /// 创建Shader目录
+    NSString* shaderFolder = [documentPath stringByAppendingFormat:@"/Shader"];
+    /// 创建目录
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    BOOL isFileExists = [fileManager fileExistsAtPath:shaderFolder];
+    if (!isFileExists)
+    {
+        [fileManager createDirectoryAtPath:shaderFolder withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    /// 得到完整的文件路径
+    NSString* strFilePath = [shaderFolder stringByAppendingFormat:@"/%s.glsl", std::to_string(++shaderIndex).c_str()];
+    /// 创建文件并将字符串写入文件
+    NSData* fileData = [NSData dataWithBytes:source.UTF8String length:source.length];
+    [fileManager createFileAtPath:strFilePath contents:fileData attributes:nil];
+}
+
+void (*glShaderSourceFunc)(GLuint shader, GLsizei count, const GLchar* const* string, const GLint* length);
+static void glShaderSourceNew(GLuint shader, GLsizei count, const GLchar* const* string, const GLint* length)
+{
+    NSString* shaderSource = [NSString stringWithFormat:@"%s", *string];
+    NSLog(@"glShaderSource(%d, %d, \n%@\n, %p) called", shader, count, shaderSource, length);
+    saveShaderToFile(shaderSource);
+    glShaderSourceFunc(shader, count, string, length);
+}
+
 
 CHConstructor{
     printf(INSERT_SUCCESS_WELCOME);
@@ -42,6 +74,7 @@ CHConstructor{
         glHook.begin();
         glHook.insertFunc("glDrawArrays", (void*)&glDrawArraysNew, (void**)&glDrawArraysFunc);
         glHook.insertFunc("glDrawElements", (void*)&glDrawElementsNew, (void**)&glDrawElementsFun);
+        glHook.insertFunc("glShaderSource", (void*)&glShaderSourceNew, (void**)&glShaderSourceFunc);
         glHook.finish();
     }];
 }
